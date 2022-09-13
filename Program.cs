8733e59a98cf3ab2c72f9bcd07ab03655aac7eca
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-09-13 18:00:09 -0500

    Conectar con base de datos en SQL Server

--- Program.cs
+++ Program.cs
@@ -4,11 +4,12 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+//builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+builder.Services.AddSqlServer<TareasContext>("Data Source=127.0.0.1; Initial Catalog=entity; user=root; password=123");
 
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/dbconexion", async([FromServices] TareasContext dbContext) =>
 {
