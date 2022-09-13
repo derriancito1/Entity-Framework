4b9c43243d9b032e202bbbea2b274f69d0f9767c
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-09-13 13:18:31 -0500

    Mapeo de modelos

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace Entity_Framework.Models;
 
 public class Categoria
 {
+    [Key]
     public Guid CategoriaId {get;set;}
+    [Required]
+    [MaxLength(150)]
     public string Nombre {get;set;}
     public string Descripcion {get;set;}
     public virtual ICollection<Tarea> Tareas {get;set;}
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +9,21 @@ namespace Entity_Framework.Models;
 
 public class Tarea
 {
+    [Key]
     public Guid TareaId {get;set;}
+
+    [ForeignKey("CategoriaID")]
     public Guid CategoriaId {get;set;}
+    [Required]
+    [MaxLength(200)]
     public string Titulo {get;set;}
     public string Descripcion {get;set;}
     public Prioridad PrioridadTarea {get;set;}
     public DateTime FechaCreacion {get;set;}
     public virtual Categoria Categoria {get;set;}
+
+    [NotMapped]
+    public string Resumen {get;set;}
 }
 
 public enum Prioridad 
