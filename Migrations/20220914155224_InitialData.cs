20798aae9989c18aa59128fb229303dc8331b25f
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-09-14 10:54:37 -0500

    Campos no requeridos y agregar data en la database

---
+++ Migrations/20220914155224_InitialData.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_Framework.Migrations
+{
+    public partial class InitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tarea",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Categoria",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Categoria",
+                columns: new[] { "CategoriaId", "Descripcion", "Nombre", "Peso" },
+                values: new object[] { new Guid("91c48411-08ed-4964-aa46-35af87aa8a01"), null, "Actividades Personales", 50 });
+
+            migrationBuilder.InsertData(
+                table: "Categoria",
+                columns: new[] { "CategoriaId", "Descripcion", "Nombre", "Peso" },
+                values: new object[] { new Guid("91c48411-08ed-4964-aa46-35af87aa8a3d"), null, "Actividades Pendientes", 20 });
+
+            migrationBuilder.InsertData(
+                table: "Tarea",
+                columns: new[] { "TareaId", "CategoriaId", "Descripcion", "FechaCreacion", "PrioridadTarea", "Titulo" },
+                values: new object[] { new Guid("91c48411-08ed-4964-aa46-35af87aa8a10"), new Guid("91c48411-08ed-4964-aa46-35af87aa8a3d"), null, new DateTime(2022, 9, 14, 10, 52, 24, 306, DateTimeKind.Local).AddTicks(9044), 1, "Pago de servicios publicos" });
+
+            migrationBuilder.InsertData(
+                table: "Tarea",
+                columns: new[] { "TareaId", "CategoriaId", "Descripcion", "FechaCreacion", "PrioridadTarea", "Titulo" },
+                values: new object[] { new Guid("91c48411-08ed-4964-aa46-35af87aa8a11"), new Guid("91c48411-08ed-4964-aa46-35af87aa8a01"), null, new DateTime(2022, 9, 14, 10, 52, 24, 306, DateTimeKind.Local).AddTicks(9092), 0, "Terminar de ver pelicula en Netflix" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tarea",
+                keyColumn: "TareaId",
+                keyValue: new Guid("91c48411-08ed-4964-aa46-35af87aa8a10"));
+
+            migrationBuilder.DeleteData(
+                table: "Tarea",
+                keyColumn: "TareaId",
+                keyValue: new Guid("91c48411-08ed-4964-aa46-35af87aa8a11"));
+
+            migrationBuilder.DeleteData(
+                table: "Categoria",
+                keyColumn: "CategoriaId",
+                keyValue: new Guid("91c48411-08ed-4964-aa46-35af87aa8a01"));
+
+            migrationBuilder.DeleteData(
+                table: "Categoria",
+                keyColumn: "CategoriaId",
+                keyValue: new Guid("91c48411-08ed-4964-aa46-35af87aa8a3d"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tarea",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Categoria",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
